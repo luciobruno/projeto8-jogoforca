7d7060a79ed191e1869333006edec30f2aeb9cc9
Lucio <lucio.bruno.printes@hotmail.com>
2023-02-26 22:23:55 -0300

    Bônus: chutar palavra

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/style.css";
 import React from "react";
 import Letras from "./Letras";
 import Jogo from "./Jogo";
+import Chute from "./Chute";
 
 export default function App() {
 
@@ -12,11 +13,13 @@ export default function App() {
   const [palavraSeparada,setPalavraSeparada] = React.useState([]);
   const [underlines,setUnderlines] = React.useState([]);
   const [vitoriaOuDerrota,setVitoriaOuDerrota] = React.useState("");
+  const [valor, setValor] = React.useState('');
 
   return (
     <>
       <Jogo setVitoriaOuDerrota={setVitoriaOuDerrota} vitoriaOuDerrota={vitoriaOuDerrota} setHabilitarButton={setHabilitarButton} error={error} setError={setError} setHabilitado={setHabilitado} palavraSeparada={palavraSeparada} setPalavraSeparada={setPalavraSeparada} underlines={underlines} setUnderlines={setUnderlines}/>
       <Letras setVitoriaOuDerrota={setVitoriaOuDerrota} habilitarButton={habilitarButton} setHabilitarButton={setHabilitarButton} setError={setError} error={error} habilitado={habilitado} setHabilitado={setHabilitado} palavraSeparada={palavraSeparada} setUnderlines={setUnderlines} underlines={underlines}/>
+      <Chute setHabilitarButton={setHabilitarButton} setError={setError} setUnderlines={setUnderlines} setVitoriaOuDerrota={setVitoriaOuDerrota} palavraSeparada={palavraSeparada} valor={valor} setValor={setValor} habilitarButton={habilitarButton}/>
     </>
   );
 }
---
+++ src/Chute.js
@@ -0,0 +1,34 @@
+export default function Chute({setHabilitarButton,setError,setUnderlines,setVitoriaOuDerrota,palavraSeparada,habilitarButton,valor,setValor}){
+
+    const handleChange = (event) => {
+        setValor(event.target.value);
+        };
+        
+    const palavraChutada = valor.split('');
+    
+    function chute(){
+
+        if(compareArrays() === true){
+            setUnderlines(palavraSeparada);
+            setVitoriaOuDerrota("ganhou");
+            setHabilitarButton(true);
+        }else{
+            setUnderlines(palavraSeparada);
+            setVitoriaOuDerrota("perdeu");
+            setHabilitarButton(true);
+            setError(6);
+        }
+    }
+
+    function compareArrays() {
+        return palavraSeparada.every((value, index) => value === palavraChutada[index]);
+    }
+
+    return(
+        <div className="chute">
+            <div>Já sei a palavra!</div>
+            <input disabled={habilitarButton} onChange={handleChange} className="input"></input>
+            <button onClick={chute} className="chutar">Chutar</button>
+        </div>
+    )
+}
